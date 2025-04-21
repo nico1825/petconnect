c71a5c40417d219b598c3ea0f60ad1bd47f1a986
Nicolas Gutierrez <nicog131200@hotmail.com>
2025-04-21 01:47:58 -0400

    Fixed backend

--- app/services/api.js
+++ app/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Replace with your local IP if testing on a physical device
-const API_URL = 'https://petconnect-backend.username.repl.co';
+const API_URL = 'https://petconnect-ulvh.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
